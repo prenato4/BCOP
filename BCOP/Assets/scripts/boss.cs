ebe386d1e2048aa3a5b7cbd3fbfdc17f26ba5f12
Olga Tindô da Rocha Barbosa <20211170150002@ifrn.local>
2023-10-02 11:09:24 -0200

    uaua

--- BCOP/Assets/scripts/boss.cs
+++ BCOP/Assets/scripts/boss.cs
@@ -5,29 +5,35 @@ using UnityEngine;
 public class boss : MonoBehaviour
 {
     public float speed;
-    public float walktime;
-    private Rigidbody2D rig;
+    public float walkTime;
+    public int health;
+    public int damage = 1;
+
+    private Animator anim;
     private float timer;
-    private bool walkright = false;
-    
+    private bool walkRight;
+
+    private Rigidbody2D rig;
+
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
-    { 
-        timer = Time.deltaTime;
+    {
+        timer += Time.deltaTime;
 
-        if (timer >= walktime)
+        if (timer >= walkTime)
         {
-            walkright = !walkright;
+            walkRight = !walkRight;
             timer = 0f;
         }
 
-        if (walkright)
+        if (walkRight)
         {
             transform.eulerAngles = new Vector2(0, 0);
             rig.velocity = Vector2.right * speed;
@@ -37,5 +43,24 @@ public class boss : MonoBehaviour
             transform.eulerAngles = new Vector2(0, 180);
             rig.velocity = Vector2.left * speed;
         }
+    }
+
+    public void Damage(int dmg)
+    {
+        health -= dmg;
+        //anim.SetTrigger("hit");
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //collision.gameObject.GetComponent<player>().Damage(damage);
+        }    
     }
 }
