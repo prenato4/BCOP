990c22fedcad4e228e03ae6a987aa006e14a0263
BRENNA MARIA DA SILVA LIMA <s.brenna@academico.ifrn.edu.br>
2023-10-16 11:50:37 -0300

    script1

---
+++ BCOP/Assets/boss1/script/boss1.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    public float speed;
+    public float walkTime;
+
+    private float timer;
+    private bool walkRight;
+
+    private Rigidbody2D rig;
+    public Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= walkTime)
+        {
+            walkRight = walkRight;
+            timer = 0f;
+        }
+
+        if (walkRight)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            rig.velocity = Vector2.right * speed;
+        }
+
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            rig.velocity = Vector2.left * speed;
+        }
+        anim.SetInteger("transition, 0");
+    }
+}
