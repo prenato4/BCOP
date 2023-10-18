b565e14488a820aa8999b58ea277910908e85a6a
CLARA NARIELY DE MELO ALVES <20211170150067@ifrn.local>
2023-10-18 13:47:59 -0300

    andando

--- BCOP/Assets/scripts/Fairy.cs
+++ BCOP/Assets/scripts/Fairy.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Fairy : MonoBehaviour
 {
     public float speed;
+    public float walkTime;
+
+    private float timer;
+    private bool walkRight = true;
 
     private Rigidbody2D rig;
     
@@ -17,6 +21,23 @@ public class Fairy : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rig.velocity = Vector2.right * speed;
+        timer += Time.deltaTime;
+        
+        if (timer >= walkTime)
+        {
+            walkRight = !walkRight;
+            timer = 0f;
+        }
+
+        if (walkRight)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+             rig.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            rig.velocity = Vector2.left * speed;
+        }
     }
 }
