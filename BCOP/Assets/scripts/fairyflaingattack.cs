9b769d07b3fa259370461e01e7971e78ecbbf92c
CLARA NARIELY DE MELO ALVES <20211170150067@ifrn.local>
2023-10-18 14:09:46 -0300

    amoqamodeucrttt

--- BCOP/Assets/scripts/fairyflaingattack.cs
+++ BCOP/Assets/scripts/fairyflaingattack.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class fairyflaingattack : MonoBehaviour
 {
+    public float speed;
+    public float flayngTime;
+
+    private float timer;
+    private bool flayngRight = true;
+
+    private Rigidbody2D rig;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= flayngTime)
+        {
+            flayngRight = !flayngRight;
+            timer = 0f;
+        }
+
+        if (flayngRight)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            rig.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            rig.velocity = Vector2.left * speed;
+        }
     }
 }
