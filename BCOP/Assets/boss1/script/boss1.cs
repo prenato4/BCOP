2f9483931b39a93dcfeb83d6f7376ba71580bc72
BRENNA MARIA DA SILVA LIMA <s.brenna@academico.ifrn.edu.br>
2023-11-06 16:10:29 -0300

    tentei

--- BCOP/Assets/boss1/script/boss1.cs
+++ BCOP/Assets/boss1/script/boss1.cs
@@ -1,15 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class boss1 : MonoBehaviour
 {
     public float speed;
     public float walkTime;
+    public GameObject tiro;
+    public Transform firepoint;
+    public bool stage;
+    public float vida = 20;
+
+    public float velocity;
 
     private float timer;
     private bool walkRight;
+    private bool isFire;
 
     private Rigidbody2D rig;
     public Animator anim;
@@ -22,6 +31,15 @@ public class boss1 : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    private void Update()
+    {
+        if (vida <= 10)
+        {
+            stage = true;
+        }
+    }
+
     void FixedUpdate()
     {
         timer += Time.deltaTime;
@@ -44,6 +62,25 @@ public class boss1 : MonoBehaviour
             rig.velocity = Vector2.left * speed;
         }
 
-        
+        void attack()
+        {
+            if (stage == false)
+            {
+               anim.SetInteger("Transition", 2 );
+               GameObject bullet = Instantiate(tiro, firepoint.position, firepoint.rotation);
+               Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+               rb.velocity = firepoint.right * velocity;
+               Destroy(bullet, 2f);
+            }
+        }
+
+        void attack2()
+        {
+            if (stage == true)
+            {
+                
+            }
+        }
     }
+    
 }
