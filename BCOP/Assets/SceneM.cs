dac5c6f15c72d805d055f6ac718993752a9792c3
Pedro Renato Dos Anjos Silva <20211170150017@ifrn.local>
2023-11-08 13:55:32 -0300

    FFFFFFF

---
+++ BCOP/Assets/SceneM.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneM : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}
