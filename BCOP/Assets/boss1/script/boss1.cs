f89180251f2378eec2b47b43e4c83e67ab921494
BRENNA MARIA DA SILVA LIMA <s.brenna@academico.ifrn.edu.br>
2023-11-08 11:55:39 -0300

    15:02

--- BCOP/Assets/boss1/script/boss1.cs
+++ BCOP/Assets/boss1/script/boss1.cs
@@ -1,97 +1,29 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class boss1 : MonoBehaviour
+public class Boss1 : MonoBehaviour
 {
-    public float walkTime;
-    public GameObject tiro;
-    public Transform firepoint;
-    public bool stage;
-    public float velocity;
-    public int speed;
+    public Transform player;
 
-    private Rigidbody2D rig;
-    private Animator anim;
-    private int health;
-    private float timer;
-    private bool walkRight;
-    private bool enraged;
+    public bool isFlipped = false;
 
-    void Start()
+    public void LookAtPlayer()
     {
-        health = 100;
-        enraged = false;
-        anim = GetComponent<Animator>();
-        rig = GetComponent<Rigidbody2D>();
-    }
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
 
-    private void Update()
-    {
-        if (health <= 50 && !enraged)
+        if (transform.position.x > player.position.x && isFlipped)
         {
-            Enrage();
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
         }
-    }
-    
-    void Enrage()
-    {
-        enraged = true;
-    }
-
-    void FixedUpdate()
-    {
-        timer += Time.deltaTime;
-
-        if (timer >= walkTime)
+        else if (transform.position.x < player.position.x && !isFlipped)
         {
-            walkRight = !walkRight;
-            timer = 0f;
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
         }
-
-        if (walkRight)
-        {
-            transform.eulerAngles = new Vector2(0, 0);
-            rig.velocity = Vector2.right * speed;
-        }
-
-        else
-        {
-            transform.eulerAngles = new Vector2(0, 180);
-            rig.velocity = Vector2.left * speed;
-        }
-
-        void CanAttack()
-        {
-            if (stage == false)
-            {
-                anim.SetInteger("Transition", 2);
-                GameObject bullet = Instantiate(tiro, firepoint.position, firepoint.rotation);
-                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-                rb.velocity = firepoint.right * velocity;
-                Destroy(bullet, 2f);
-            }
-        }
-
-        void attack2()
-        {
-            if (stage == true)
-            {
-                IEnumerator Attack()
-                {
-                    anim.SetBool("Attack", true);
-                    speed = 0;
-
-                    yield return new WaitForSeconds(0.85f);
-                    anim.SetBool("Attack", false);
-                    speed = 1;
-                }
-            }
-        }
-
     }
 }
